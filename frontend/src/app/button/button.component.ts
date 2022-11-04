5e9c8d3c58cebf57fdc651f9a4f177d4ee2ff772
Nikolay Churkin <n.churkin@simdev.ru>
2022-11-04 13:53:58 +0300

    Confirm

---
+++ frontend/src/app/button/button.component.ts
@@ -0,0 +1,17 @@
+import {Component, Input, OnInit, ViewEncapsulation} from '@angular/core';
+
+@Component({
+  selector: 'app-button',
+  templateUrl: './button.component.html',
+  styleUrls: ['./button.component.scss'],
+  encapsulation: ViewEncapsulation.None
+})
+export class ButtonComponent implements OnInit {
+  @Input('transparent') transparent = false;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
