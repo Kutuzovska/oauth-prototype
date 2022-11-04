fe3cc63e7cfbba441d9beb991119299dc5832591
Nikolay Churkin <nc19091997@gmail.com>
2022-11-05 00:30:28 +0300

    Angular login page

--- frontend/src/app/login/login.component.ts
+++ frontend/src/app/login/login.component.ts
@@ -3,13 +3,26 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent implements OnInit {
+  email: string = '';
+
+  password: string = '';
 
   constructor() { }
 
   ngOnInit(): void {
   }
+  //
+  // login(event: Event, loginForm: NgForm) {
+  //   event.preventDefault();
+  //   console.log(this.email);
+  //   console.log(this.password);
+  //   console.log(loginForm);
+  // }
 
+  submit() {
+    console.log('submit');
+  }
 }
